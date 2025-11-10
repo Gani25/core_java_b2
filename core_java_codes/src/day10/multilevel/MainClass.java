a193d6fb8865b0b9691edceceebecb818a542d3a
Gani25 <memonabdulgani3d@gmail.com>
2025-11-10 13:12:56 +0530

    Static, Final and both

--- core_java_codes/src/day10/multilevel/MainClass.java
+++ core_java_codes/src/day10/multilevel/MainClass.java
@@ -4,9 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		PartTimeEmployee pt1 = new PartTimeEmployee("Sholk Sharma", 30, "Male", 1001, 1500, 25);
-		
 		pt1.calculateTax();
 		System.out.println(pt1);
 	}
-
 }
