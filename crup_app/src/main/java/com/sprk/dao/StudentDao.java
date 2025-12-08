406ff2b6d10978803ca9b8826ab0a99490323822
Gani25 <memonabdulgani3d@gmail.com>
2025-12-08 12:32:58 +0530

    CRUD APP JDBC

--- crup_app/src/main/java/com/sprk/dao/StudentDao.java
+++ crup_app/src/main/java/com/sprk/dao/StudentDao.java
@@ -4,57 +4,58 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 import com.sprk.model.Student;
 
 //Data Access Object Layer -> Repository
 public class StudentDao {
-	
+
 	// Aggregation
 	private Connection connection;
 
 	public StudentDao(Connection connection) throws Exception {
-		
+
 		if (connection == null) {
 			throw new Exception("Please Pass Connection To Proceed");
 		}
-		
+
 		this.connection = connection;
 	}
-	
+
 	public void testConnection() {
-		System.out.println("Connection Obj = "+connection);
+		System.out.println("Connection Obj = " + connection);
 	}
-	
+
 	public int insertStudent(Student student) throws SQLException {
-		
+
 		String sql = "insert into student(first_name, last_name, gender) values (?, ?, ?)";
-		
+
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		
+
 		preparedStatement.setString(1, student.getFirstName());
 		preparedStatement.setString(2, student.getLastName());
 		preparedStatement.setString(3, student.getGender());
-		
+
 		int result = preparedStatement.executeUpdate();
-		
+
 		closeAll(connection, preparedStatement, null);
-		
+
 		return result;
 	}
-	
-	public Student findByRollNo(int rollNo) throws Exception{
-		
+
+	public Student findByRollNo(int rollNo) throws SQLException {
+
 		String sql = "select * from student where roll_no = ?";
-		
+
 		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		
+
 		preparedStatement.setInt(1, rollNo);
-		
-		
+
 		ResultSet resultSet = preparedStatement.executeQuery();
 		Student student = null;
-		if(resultSet.next()) {
+		if (resultSet.next()) {
 			student = new Student();
 			student.setFirstName(resultSet.getString("first_name"));
 			student.setLastName(resultSet.getString("last_name"));
@@ -63,26 +64,57 @@ public class StudentDao {
 			student.setAccCreatedAt(resultSet.getTimestamp("acc_created_at"));
 			student.setAccUpdatedAt(resultSet.getTimestamp("last_update"));
 		}
-		
+
 		closeAll(connection, preparedStatement, resultSet);
 		return student;
 	}
-	
-	private void closeAll(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet) throws SQLException {
+
+	// Find All Students -> Many Rows -> Arrays[Fixed in Size]
+	// Collections -> List<Student>
+
+	public List<Student> getAllStudents() throws SQLException {
+
+		String sql = "select * from student";
+
+		PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+
+		ResultSet resultSet = preparedStatement.executeQuery();
 		
-		if(connection != null) {
+		// We have many rows -> no idea about number of rows -> while loop
+		List<Student> allStudents = new LinkedList<Student>(); 
+		// size == 0 -> No students found/ db is empty
+		
+		while(resultSet.next()) {
+			
+			Student student = new Student();
+			student.setFirstName(resultSet.getString("first_name"));
+			student.setLastName(resultSet.getString("last_name"));
+			student.setRollNo(resultSet.getInt("roll_no"));
+			student.setGender(resultSet.getString("gender"));
+			student.setAccCreatedAt(resultSet.getTimestamp("acc_created_at"));
+			student.setAccUpdatedAt(resultSet.getTimestamp("last_update"));
+			
+			allStudents.add(student);
+			
+		}
+		
+		closeAll(connection, preparedStatement, resultSet);
+		return allStudents;
+	}
+
+	private void closeAll(Connection connection, PreparedStatement preparedStatement, ResultSet resultSet)
+			throws SQLException {
+
+		if (connection != null) {
 			connection.close();
 		}
-		if(preparedStatement != null) {
+		if (preparedStatement != null) {
 			preparedStatement.close();
 		}
-		if(resultSet != null) {
+		if (resultSet != null) {
 			resultSet.close();
 		}
 	}
-	
-	
-	
-	
 
 }
